322e9c29c367e911e3cceaf7fdf8e73be437fba3
alois <alois.aubert99@gmail.com>
2020-01-01 20:47:44 +0100

    API &Laurine

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-//API TEST
-var footer = document.getElementById('typedtext');
-
-function fetchFooter(){
-
-  fetch('https://api.tronalddump.io/random/quote')
-          .then(response => {
-            response.json().then((typedtext)=>{
-              console.log(typedtext);
-            })
-          })
-  //try{
-    //fetch('https://api.tronalddump.io/random/quote')
-    //.then(response => response.text())
-  //  .then(data => {
-  //  footer.innerHTML = data;
-    //})
-  //} catch {
-  //  console.log("erreur");
-//  }
-//}
-
-
-fetchFooter();
+var quoteContainer = document.getElementById("quotes");
 
 
 
 
 
 
+//Récupere les infos de l'API+++++++++++++++++++++
+const xhr = new XMLHttpRequest();
+
+
+xhr.onload = function(){
+  if (this.status ===200){
+    try {
+      const resObj = JSON.parse(this.responseText);
+      quoteContainer.insertAdjacentHTML('beforeend',resObj.value);
+    }catch(e){
+      console.warn('There was an error in the JSON. Could not parse!')
+    }
+}else{
+  console.warn('Did not receive 200 OK from response!')
+  }
+};
+
+xhr.open('get','https://api.tronalddump.io/random/quote');
+xhr.send();
 
 
 
+//Ajoute un element html pour afficher la citations
 
 
-// inspiration : effet texte citation par gavra // pour afficher citations
-var aText = new Array (fetchFooter()); // noter citations entre parenthèse
-var iSpeed = 100; // délai d'impression
-var iIndex = 0; // position de départ
-var iArrLength = aText[0].length; // longueur du tableau de texte
-var iScrollAt = 20; // Commencement défilement
 
-var iTextPos = 0; // initialisation de la position du texte
-var sContents = ''; // initialisation de la variable
-var iRow; // initialiser la ligne
+// Code pour afficher comme un machine à écrire la citation
+/*
+// set up text to print, each item in array is new line
+var aText = new Array(
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
 
 function typewriter()
 {
@@ -59,7 +60,7 @@ function typewriter()
   iIndex++;
   if ( iIndex != aText.length ) {
    iArrLength = aText[iIndex].length;
-   setTimeout("typewriter()", 400);
+   setTimeout("typewriter()", 500);
   }
  } else {
   setTimeout("typewriter()", iSpeed);
@@ -68,136 +69,4 @@ function typewriter()
 
 
 typewriter();
-/*
-// requette https// Création d'une requête HTTP
-var req = new XMLHttpRequest();
-// Requête HTTP GET synchrone vers le fichier langages.txt publié localement
-req.open("GET", "http://localhost/javascript-web-srv/data/langages.txt", false);
-// Envoi de la requête
-req.send(null);
-// Affiche la réponse reçue pour la requête
-console.log(req.responseText);
-
-var req = new XMLHttpRequest();
-// La requête est asynchrone lorsque le 3ème paramètre vaut true ou est absent
-req.open("GET", "http://localhost/javascript-web-srv/data/langages.txt");
-// Gestion de l'événement indiquant la fin de la requête
-req.addEventListener("load", function () {
-    // Affiche la réponse reçue pour la requête
-    console.log(req.responseText);
-});
-req.send(null);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// API --> Ça ne fonctionne pas !
-
-/*fetch('https://api.tronalddump.io/random/quote')
-        .then(response => {
-          response.json().then((quotes)=>{
-            console.log(quotes);
-          })
-        })
-
-  async function quotes(){
-    /*const response = await fetch('https://planets-api-webimac2019.now.sh/');
-    const myJson = await response.json();
-    console.log(response, myJson);
-    fetch('https://api.tronalddump.io/random/quote')
-            .then(response => {
-              response.json().then((quotes)=>{
-                console.log(quotes);
-              })
-            })
-
-
-
-          myJson.quotes.forEach(function(e){
-            var quotes = document.getElementById('quotes');
-            var trackName = e.name;
-            var size = e.size;
-            var title = document.createElement("div");
-            title.innerHTML = name;
-            if (e.img){
-              title.style.background = `no-repeat url(${e.img}) center/100%`
-            }
-            title.classList.add(size);
-            quotes.appendChild(title);
-            console.log(e.name);
-          });
-        }
-
-
-
-
-        quotes();
-
-
-async function test(){
-  const response = await fetch('https://api.tronalddump.io/random/quote');
-  const myJson = await response.json();
-  console.log(response, myJson);
-
-  myJson.quotes.forEach(function(e){
-    var quotes = document.getElementById('quotes');
-    var trackName = e.name;
-    var size = e.size;
-    var title = document.createElement("div");
-    title.innerHTML = name;
-    if (e.img){
-      title.style.background = `no-repeat url(${e.img}) center/100%`
-    }
-    title.classList.add(size);
-    quotes.appendChild(title);
-    console.log(e.name);
-  });
-}
-
-
-
-
-test();
-
-
-request.open('GET', 'https://api.tronalddump.io/random/quote',true);
-request.send();
-ajaxGet("https://api.tronalddump.io/random/quote", function (reponse) {
-    console.log(reponse);
-});
-
-var citationsElt = document.getElementById("quotess");
-ajaxGet("https://api.tronalddump.io/random/quote", function (reponse) {
-    // Transforme la réponse en un tableau d'articles
-    var quotes = JSON.parse(reponse);
-    quotes.forEach(function (quote) {
-        // Ajout contenu de chaque citation
-        var contenuElt = document.createElement("p");
-        contenuElt.textContent = quotes.contenu;
-        quotesElt.appendChild(contenuElt);
-    });
-});
 */
