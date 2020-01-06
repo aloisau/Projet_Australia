9fe60bacb9be6bda70d9e91e7b297ce98a451a8b
alois <alois.aubert99@gmail.com>
2020-01-07 00:41:03 +0100

    mise en page +form

--- main.js
+++ main.js
@@ -1,57 +1,62 @@
 var quoteContainer = document.getElementById("quotes");
-
+var ans;
 //GENERATION ALEATOIRE D'UNE CITATION///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Fonction qui renvoie un nombre aléatoire, que je stocke dans i ..............................
+//Fonction qui renvoie un nombre aléatoire..............................
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-var i = getRandomInt(2);
-console.log(i);
 //.............................................................................................
 
-if (i==0){
-  var ans = 0;
+
+function start() {
+  var i = getRandomInt(2);
+  console.log(i);
+
+
+  if (i==0){
+    ans = 0;
 //Récupere les infos de l'API+++++++++++++++++++++
-const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-xhr.onload = function(){
-  if (this.status ===200){
-    try {
-      const resObj = JSON.parse(this.responseText);
-      quoteContainer.insertAdjacentHTML('beforeend',resObj.value); //Ajoute l'élément html quote
-    }catch(e){
-      console.warn('There was an error in the JSON. Could not parse!')
+    xhr.onload = function(){
+    if (this.status ===200){
+      try {
+        const resObj = JSON.parse(this.responseText);
+        quoteContainer.insertAdjacentHTML('beforeend',"<p>"+ resObj.value+"</p>" ); //Ajoute l'élément html quote
+      }catch(e){
+        console.warn('There was an error in the JSON. Could not parse!')
+      }
+    }else{
+      console.warn('Did not receive 200 OK from response!')
     }
-}else{
-  console.warn('Did not receive 200 OK from response!')
-  }
-};
+  };
 
-xhr.open('get','https://api.tronalddump.io/random/quote');
-xhr.send();
+    xhr.open('get','https://api.tronalddump.io/random/quote');
+    xhr.send();
 //----------------------------------------------------
-}
-else{
-  var ans = 1;
-const xhr1 = new XMLHttpRequest();
-
-xhr1.onload = function(){
-  if (this.status ===200){
-    try {
-      const resObj1 = JSON.parse(this.responseText);
-      console.log(resObj1);
-      quoteContainer.insertAdjacentHTML('beforeend',resObj1.quoteText); //Ajoute l'élément html quote
-    }catch(e){
-      console.warn('There was an error in the JSON. Could not parse!')
-    }
-}else{
-  console.warn('Did not receive 200 OK from response!')
   }
-};
+  else{
+    ans = 1;
+    const xhr1 = new XMLHttpRequest();
 
-xhr1.open('get','https://quote-garden.herokuapp.com/quotes/random');
-xhr1.send();
+    xhr1.onload = function(){
+      if (this.status ===200){
+        try {
+          const resObj1 = JSON.parse(this.responseText);
+          console.log(resObj1);
+          quoteContainer.insertAdjacentHTML('beforeend',"<p>"+resObj1.quoteText+"</p>"); //Ajoute l'élément html quote
+        }catch(e){
+          console.warn('There was an error in the JSON. Could not parse!')
+        }
+      }else{
+        console.warn('Did not receive 200 OK from response!')
+      }
+    };
+
+    xhr1.open('get','https://quote-garden.herokuapp.com/quotes/random');
+    xhr1.send();
+  }
 }
 
 
@@ -59,44 +64,32 @@ xhr1.send();
 
 
 
+var ul=document.getElementById('ul');
+var btn=document.getElementById('button');
+var yes=document.getElementById('yes0');
+var no=document.getElementById('no1');
+var nextButton=document.getElementById('nextButton');
 
-  var ul=document.getElementById('ul');
-  var btn=document.getElementById('button');
- var yes=document.getElementById('yes0');
- var no=document.getElementById('no1');
-
-
-     var app={
-               question:{options:['Yes','No'],answer:ans}
-              ,
+     var game={
                load:function(){
-                       yes.innerHTML=this.question.options[0];
-                       no.innerHTML=this.question.options[1];
+                       start();
+                       ul.insertAdjacentHTML('beforeend','<li id="yes0" onclick="button(this)">Yes</li>' );
+                       ul.insertAdjacentHTML('beforeend','<li id="no1" onclick="button(this)">No</li>' );
                },
-                next:function(){
-                   //document.reload(forcedReload);
-                   console.log('Yes');
-                   document.location.reload(true);
-                },
+               next:function(){
+                  this.load();
+               },
                check:function(ele){
 
                         var id=ele.id.split('');
 
-                        if(id[id.length-1]==this.question.answer){
-                         ele.className="correct";
-                         ele.innerHTML="Correct";
+                        if(id[id.length-1]==ans){
+
                         }
                         else{
-                         ele.className="wrong";
-                         ele.innerHTML="Wrong";
+
                         }
                },
-               notClickAble:function(){
-                      for(let i=0;i<ul.children.length;i++){
-                           ul.children[i].style.pointerEvents="none";
-                      }
-               },
-
                clickAble:function(){
                       for(let i=0;i<ul.children.length;i++){
                            ul.children[i].style.pointerEvents="auto";
@@ -109,19 +102,69 @@ xhr1.send();
           }
 
 
-          window.onload=app.load();
+          window.onload=game.load();
 
           function button(ele){
-                app.check(ele);
-                app.notClickAble();
+                game.check(ele);
+                while (ul.firstChild) {
+                   ul.removeChild(ul.firstChild);
+                }
+                quoteContainer.removeChild(quoteContainer.firstChild);
+                nextButton.insertAdjacentHTML('beforeend','<button type="button" onclick="next()" id="button1">Start again!</button>' );
+                nextButton.children[0].style.pointerEvents="auto";
           }
 
-        function  next(){
-             app.next();
-             app.clickAble();
+          function  next(){
+             nextButton.removeChild(nextButton.firstChild);
+             window.onload=game.load();
+             game.clickAble();
+          }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Formulaire
+
+function validate(){
+  var quote = document.getElementById("quote").value;
+  var error_message = document.getElementById("error_message");
+
+  error_message.style.padding = "10px";
+
+  var text;
+  if(quote.length < 5){
+    text = "Please Enter valid quote";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
+
+
+
+
+
+
+        const addUserQuotes = (ev)=>{
+            ev.preventDefault();  //to stop the form submitting
+            localStorage.setItem("quote"+localStorage.length, JSON.stringify(quote.value) );
+            document.forms[0].reset(); // to clear the form for the next entries
+            //saving to localStorage
         }
+        document.addEventListener('DOMContentLoaded', ()=>{
+            document.getElementById('button').addEventListener('click', addUserQuotes);
+        });
+
+var number = 0;
+var userquote = localStorage.getItem ('quote'+number);
+
+console.log(userquote)
+
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
 
 
 
